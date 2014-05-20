6e77df033e6877d3af04b52d219a62db7e9dad2c
Brandon Valosek <bvalosek@gmail.com>
2014-05-20 15:35:32 -0500

    OpenShift support

--- README.md
+++ README.md
@@ -2,5 +2,35 @@
 
 dat stack tho
 
+## Awesome
 
+Install dependencies:
+
+```
+$ npm install
+```
+
+Build the client application:
+
+```
+$ npm build
+```
+
+Run tests (if there are any):
+
+```
+$ npm test
+```
+
+Run database migrations (if there are any):
+
+```
+$ npm migrate
+```
+
+Start the server app:
+
+```
+$ npm start
+```
 
--- app/server/config/http.js
+++ app/server/config/http.js
@@ -3,9 +3,10 @@ var path = require('path');
 module.exports = {
 
   // Add logic here to handle PaaS deployments or other proxying solutions
-  port      : 8123,
-  ipAddress : '127.0.0.1',
+  port      : process.env.OPENSHIFT_NODEJS_PORT || 8123,
+  ipAddress : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 
+  // Static file serving
   webroot   : path.resolve('./dist/debug')
 
 };
